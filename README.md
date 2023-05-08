598e0cd2cd319cc8aabd504b7d02e31280889ca8
mfux <maxfuchs@protonmail.com>
2023-05-08 11:45:18 +0200

    setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ask PDF
+
+Learning project followed from this tutorial [Video](https://www.youtube.com/watch?v=wUAUdEw5oxM)
+
+---
+
+---
---
+++ app.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+def main():
+    # load api key
+    load_dotenv()
+
+
+if __name__ == "__main__":
+    main()
