7254ad75c4f075281d3816c115a936d4d6ff5604
mfux <maxfuchs@protonmail.com>
2023-05-08 12:27:09 +0200

    split pdf into chunks and start knowledge base

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def main():
@@ -17,11 +20,28 @@ def main():
     if pdf:
         # create new pdf reader
         pdf_reader = PdfReader(pdf)
+        # read pdf
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        st.write(text)
+        # split text into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+
+        # create knowledge base
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+
+
 
 if __name__ == "__main__":
     main()
