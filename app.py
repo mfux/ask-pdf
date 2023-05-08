1acf679bcced1207823a6d1219a32f22cc667c2a
mfux <maxfuchs@protonmail.com>
2023-05-08 11:56:40 +0200

    create gui

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 
 def main():
     # load api key
     load_dotenv()
+    # design page
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF ☁️")
+
+    # add uploader widget
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
 
 
 if __name__ == "__main__":
